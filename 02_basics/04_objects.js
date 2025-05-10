0905ea1edad888c65306890be1e5e25a60c44fc1
AdityaSalian011 <adityasalian865@gmail.com>
2025-05-10 12:33:27 +0000

    json and destructor in objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -84,16 +84,41 @@ const users = [
 // console.log(users[1].userID, users[1].email);
 // console.log(users[2].userID, users[2].email);
 
-console.log(instaUser);
+// console.log(instaUser);
 
 // to obtain every key inside an object we can use Object.keys
-console.log(Object.keys(instaUser));
+// console.log(Object.keys(instaUser));
 // to obtain values of every key of an object we can user Object.values
-console.log(Object.values(instaUser));
+// console.log(Object.values(instaUser));
 
 // Returns an array of key value pairs
-console.log(Object.entries(instaUser));
+// console.log(Object.entries(instaUser));
 
 // to check if an key exists we can use
-console.log(instaUser.hasOwnProperty('isLoggedIn'));
-console.log(instaUser.hasOwnProperty('isLoggedOut'));
+// console.log(instaUser.hasOwnProperty('isLoggedIn'));
+// console.log(instaUser.hasOwnProperty('isLoggedOut'));
+
+
+// destructuring in JavaScript
+const course = {
+    courseName: 'JS in 50 days',
+    coursePrice: 999,
+    courseInstructor: 'hitesh',
+}
+
+console.log(course.courseInstructor);
+console.log(course.courseName);
+console.log(course.coursePrice);
+
+
+// destructuring course.courseInstructor into courseInstructor
+const {courseInstructor} = course
+console.log(courseInstructor);
+
+const {courseInstructor: teacher} = course  // giving alternate name for courseInstructor
+console.log(teacher);
+
+
+console.log(course.courseInstructor);  // works fine
+console.log(course.teacher);  // undefined
+
